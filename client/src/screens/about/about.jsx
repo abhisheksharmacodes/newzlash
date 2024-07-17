eb04851c8570c0c9e76184f5f2020a4c5c6e5f14
Abhishek Sharma <geekysharma31@gmail.com>
2024-07-17 17:03:26 +0530

    Minor improvements

--- client/src/screens/about/about.jsx
+++ client/src/screens/about/about.jsx
@@ -27,6 +27,7 @@ const About = () => {
                     <div id="about_info" className="our_flex">
                         In an age of information overload, staying informed on specific sustainability issues can be a challenge. Newzlash addresses this by providing a user-centric news aggregation platform. We curate content tailored to your chosen sustainability niche, empowering you to make informed decisions within your area of interest.  By filtering out extraneous information, Newzlash fosters a streamlined and efficient news experience, allowing you to delve deeper into the subjects that matter most.
                     </div>
+                    <button onClick={()=>navigate('/dashboard')} id="back" className="secondary_button">Back</button>
                 </div>
             </div>
 
